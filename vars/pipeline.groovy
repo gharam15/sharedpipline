c5948382a5c9256808bf95604067f9d4961c1336
gharam15 <garamahmedmokhtar@gmail.com>
2025-05-27 01:09:48 +0300

    add stage

---
+++ vars/pipeline.groovy
@@ -0,0 +1,28 @@
+def call() {
+    pipeline {
+         agent {
+        label "java"
+        }
+
+        environment {
+            XYZ = 'ITI ITI ITI'
+            IMAGE_NAME = 'gharam/java-iti'
+        }
+
+       stages {
+        stage('Build Java & Docker') {
+            steps {
+                echo 'Building Java app and Docker image'
+                buildJavaApp(env.IMAGE_NAME)
+            }
+        }
+
+            stage('Push Docker Image') {
+                steps {
+                    echo 'Pushing Docker image to DockerHub'
+                    pushDockerImage(env.IMAGE_NAME)
+                }
+            }
+        }
+    }
+}
